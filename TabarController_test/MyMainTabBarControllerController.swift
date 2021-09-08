d66caeb8979c6b1a265f78d7cd3c1efdf5693634
kimjunsoo <rlawnstn01023@naver.com>
2021-09-08 14:15:33 +0900

    TabBarController를 이용해서 화면 색상 전환하기

--- TabarController_test/MyMainTabBarControllerController.swift
+++ TabarController_test/MyMainTabBarControllerController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyMainTabBarControllerController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
